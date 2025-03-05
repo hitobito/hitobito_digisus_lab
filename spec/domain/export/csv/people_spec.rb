0f4e9cfff945a82e5de7a82d636e0deecc6d4ad6
Andreas Maierhofer <maierhofer@puzzle.ch>
2025-03-05 13:02:11 +0100

    Adapt to structured addresses (hitobito/hitobito#2226)

--- spec/domain/export/csv/people_spec.rb
+++ spec/domain/export/csv/people_spec.rb
@@ -10,54 +10,54 @@
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_pbs.
 
-require 'spec_helper'
-require 'csv'
+require "spec_helper"
+require "csv"
 
-describe 'Export::Csv::People' do
+describe "Export::Csv::People" do
   let(:person) { people(:admin) }
   let(:simple_headers) do
-    ['Vorname', 'Nachname', 'Übername', 'Organis.-Name', 'Kontakt:Organisation?',
-     'Haupt-E-Mail', 'Adresse', 'PLZ', 'Ort', 'Land',
-     'Hauptebene', 'Rollen', 'Titel', 'Anrede', 'Amt/Abteilung/Bereich', 'Funktion/Position']
+    ["Vorname", "Nachname", "Übername", "Organis.-Name", "Kontakt:Organisation?",
+      "Haupt-E-Mail", "zusätzliche Adresszeile", "Strasse", "Hausnummer", "Postfach", "PLZ", "Ort", "Land",
+      "Hauptebene", "Rollen", "Titel", "Anrede", "Amt/Abteilung/Bereich", "Funktion/Position"]
   end
 
   let(:list) { Person.where(id: person) }
   let(:data) { Export::Tabular::People::PeopleAddress.export(:csv, list) }
-  let(:data_without_bom) { data.gsub(Regexp.new("^#{Export::Csv::UTF8_BOM}"), '') }
+  let(:data_without_bom) { data.gsub(Regexp.new("^#{Export::Csv::UTF8_BOM}"), "") }
   let(:csv) { CSV.parse(data_without_bom, headers: true, col_sep: Settings.csv.separator) }
 
   subject { csv }
 
   before do
-    person.update!(title: 'Dr.', salutation: 'sie_m', town: 'Bern')
+    person.update!(title: "Dr.", salutation: "sie_m", town: "Bern")
   end
 
-  context 'export' do
+  context "export" do
     its(:headers) { should == simple_headers }
 
-    context 'first row' do
+    context "first row" do
       subject { csv[0] }
 
-      its(['Vorname']) { should eq person.first_name }
-      its(['Nachname']) { should eq person.last_name }
-      its(['Haupt-E-Mail']) { should eq person.email }
-      its(['Ort']) { should eq person.town }
-      its(['Rollen']) { should eq 'Administrator Verband' }
-      its(['Titel']) { should eq 'Dr.' }
-      its(['Anrede']) { should eq 'Sehr geehrter Herr' }
+      its(["Vorname"]) { should eq person.first_name }
+      its(["Nachname"]) { should eq person.last_name }
+      its(["Haupt-E-Mail"]) { should eq person.email }
+      its(["Ort"]) { should eq person.town }
+      its(["Rollen"]) { should eq "Administrator Verband" }
+      its(["Titel"]) { should eq "Dr." }
+      its(["Anrede"]) { should eq "Sehr geehrter Herr" }
     end
   end
 
-  context 'export_full' do
-    its(:headers) { should include('Titel') }
-    its(:headers) { should include('Anrede') }
+  context "export_full" do
+    its(:headers) { should include("Titel") }
+    its(:headers) { should include("Anrede") }
     let(:data) { Export::Tabular::People::PeopleFull.export(:csv, list) }
 
-    context 'first row' do
+    context "first row" do
       subject { csv[0] }
 
-      its(['Titel']) { should eq 'Dr.' }
-      its(['Anrede']) { should eq 'Sehr geehrter Herr' }
+      its(["Titel"]) { should eq "Dr." }
+      its(["Anrede"]) { should eq "Sehr geehrter Herr" }
     end
   end
 end
